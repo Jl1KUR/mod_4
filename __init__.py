dd85da902c54617c68c1907831fa5223be770e70
Пётр <petaturovnikov@gmail.com>
2023-06-15 23:41:43 +0300

    tim

---
+++ __init__.py
@@ -0,0 +1,20 @@
+'''def sTr(s):
+    for sum in set(s):
+        counter = 0
+        for sub_sum in s:
+            if sum == sub_sum:
+                counter += 1
+        print(sum, counter)
+
+sTr("hfghjkdovjvjjdkjdo")'''
+
+
+
+def sTr(s):
+    sus = {}
+    for sum in (s):
+        sus[sum] = sus.get(sum, 0) + 1
+
+    for sum, count in sus.items() :
+        print(sum, count)
+sTr("hfghjkdovjvjjdkjdo")
